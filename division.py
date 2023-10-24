576660c030ec7531e5c0b9d5110c0b5f302d1a67
Santi12G <santiagogrande87@gmail.com>
2023-10-23 23:38:33 -0500

    --all

--- division.py
+++
@@ -1,4 +0,0 @@
-def divide(num_1,num_2):
-    result =(num_1/num_2)
-    print(("%d dividided by %d is equal to %d")%(num_1,num_2,result))
-    return result
--- multiplicacion.py
+++
@@ -1,5 +0,0 @@
-
-def multiply(num_1,num_2):
-    result =(num_1 * num_2)
-    print(("%d X %d is equal to %d")%(num_1,num_2,result))
-    return result
