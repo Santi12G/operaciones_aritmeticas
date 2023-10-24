31c2bb3b8f05f7c2dfa2c8a3b62de0f7d4291f9f
Santi12G <santiagogrande87@gmail.com>
2023-10-23 23:35:17 -0500

    operations2.py

---
+++ operations2.py
@@ -0,0 +1,24 @@
+def subtract(num_1,num_2):
+    result = num_1 - num_2
+    print(("%d + % d is equal to %d")%(num_1,num_2,result))
+    return result
+
+def multiply(num_1,num_2):
+    result =(num_1 * num_2)
+    print(("%d X %d is equal to %d")%(num_1,num_2,result))
+    return result
+
+def divide(num_1,num_2):
+    result =(num_1/num_2)
+    print(("%d dividided by %d is equal to %d")%(num_1,num_2,result))
+    return result
+
+def power(num_1,num_2):
+    result =(num_1 ** num_2)
+    print(("%d raised to the power of %d is equal to %d")%(num_1,num_2,result))
+    return result
+
+def module(num_1,num_2):
+    result =(num_1 % num_2)
+    print(("%d moduled by %d is equal to %d")%(num_1,num_2,result))
+    return result
